ae55448ef462efe74385f599c9ee35efd3015b73
Eric Palacios <epalacios@datasalt.com>
2011-11-14 16:47:06 +0100

    added license in all files

--- src/main/java/com/datasalt/pangolin/mapred/package-info.java
+++ src/main/java/com/datasalt/pangolin/mapred/package-info.java
@@ -1,4 +1,20 @@
 /**
+ * Copyright [2011] [Datasalt Systems S.L.]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * The sub-package "joiner" contains a generic Multi-type Joiner that can be used to implement Map/Reduce Jobs that read
  * input data from several data sources and need to join them in the Reducer according to some grouping.
  **/
