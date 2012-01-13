b908476659445ddc0faa322fefa320961060cab9
Eric Palacios <epalace@gmail.com>
2012-01-13 13:01:17 +0100

    added SourcedTuple. Draft of serialization

---
+++ core/src/main/java/com/datasalt/pangool/io/tuple/SourcedTuple.java
@@ -0,0 +1,196 @@
+package com.datasalt.pangool.io.tuple;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+import com.datasalt.pangolin.grouper.Schema;
+import com.datasalt.pangolin.grouper.io.tuple.ITuple;
+
+public class SourcedTuple implements ITuple{
+
+	private ITuple containedTuple;
+	private int sourceId;
+	
+	public SourcedTuple(ITuple containedTuple){
+		this.containedTuple = containedTuple;
+	}
+	
+	void setContainedTuple(ITuple tuple){
+		this.containedTuple = tuple;
+	}
+	
+	int getSource(){
+		return sourceId;
+	}
+	
+	void setSource(int sourceId){
+		this.sourceId = sourceId;
+	}
+	
+	
+	
+	
+	@Override
+	public void clear() {
+		containedTuple.clear();
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		return containedTuple.containsKey(key);
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		return containedTuple.containsValue(value);
+	}
+
+	@Override
+	public Set<java.util.Map.Entry<String, Object>> entrySet() {
+		return containedTuple.entrySet();
+	}
+
+	@Override
+	public Object get(Object key) {
+		return containedTuple.get(key);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return containedTuple.isEmpty();
+	}
+
+	@Override
+	public Set<String> keySet() {
+		return containedTuple.keySet();
+	}
+
+	@Override
+	public Object put(String key, Object value) {
+		return containedTuple.put(key, value);
+	}
+
+	@Override
+	public void putAll(Map<? extends String, ? extends Object> arg0) {
+		containedTuple.putAll(arg0);
+	}
+
+	@Override
+	public Object remove(Object arg0) {
+		return containedTuple.remove(arg0);
+	}
+
+	@Override
+	public Collection<Object> values() {
+		return containedTuple.values();
+	}
+
+	@Override
+	public int compareTo(ITuple arg0) {
+		return containedTuple.compareTo(arg0);
+	}
+
+	@Override
+	public int partialHashCode(String[] fields) {
+		return containedTuple.partialHashCode(fields);
+	}
+
+	@Override
+	public int size() {
+		return containedTuple.size();
+	}
+
+	@Override
+	public int getInt(String fieldName) {
+		return containedTuple.getInt(fieldName);
+	}
+
+	@Override
+	public long getLong(String fieldName) {
+		return containedTuple.getLong(fieldName);
+	}
+
+	@Override
+	public float getFloat(String fieldName) {
+		return containedTuple.getFloat(fieldName);
+	}
+
+	@Override
+	public double getDouble(String fieldName) {
+		return containedTuple.getDouble(fieldName);
+	}
+
+	@Override
+	public String getString(String fieldName) {
+		return containedTuple.getString(fieldName);
+	}
+
+	@Override
+	public Object getObject(String fieldName) {
+		return containedTuple.getObject(fieldName);
+	}
+
+	@Override
+	public <T> T getObject(Class<T> clazz, String fieldName) {
+		return containedTuple.getObject(clazz,fieldName);
+	}
+
+	@Override
+	public Enum<? extends Enum<?>> getEnum(String fieldName) {
+		return containedTuple.getEnum(fieldName);
+	}
+
+	@Override
+	public void setEnum(String fieldName, Enum<? extends Enum<?>> value) {
+		containedTuple.setEnum(fieldName,value);
+	}
+
+	@Override
+	public void setInt(String fieldName, int value) {
+		containedTuple.setInt(fieldName,value);
+	}
+
+	@Override
+	public void setString(String fieldName, String value) {
+		containedTuple.setString(fieldName,value);
+	}
+
+	@Override
+	public void setLong(String fieldName, long value) {
+		containedTuple.setLong(fieldName,value);
+		
+	}
+
+	@Override
+	public void setFloat(String fieldName, float value) {
+		containedTuple.setFloat(fieldName,value);
+		
+	}
+
+	@Override
+	public void setDouble(String fieldName, double value) {
+		containedTuple.setDouble(fieldName,value);
+	}
+
+	@Override
+	public void setBoolean(String fieldName, boolean value) {
+		containedTuple.setBoolean(fieldName,value);
+	}
+
+	@Override
+	public void setObject(String fieldName, Object object) {
+		containedTuple.setObject(fieldName,object);
+	}
+
+	@Override
+	public <T> void setObject(Class<T> valueType, String fieldName, T value) {
+		containedTuple.setObject(valueType,fieldName,value);
+	}
+
+	@Override
+	public String toString(Schema schema, int minFieldIndex, int maxFieldIndex) {
+		return containedTuple.toString(schema,minFieldIndex,maxFieldIndex);
+	}
+
+}
---
+++ core/src/main/java/com/datasalt/pangool/io/tuple/SourcedTupleSerialization.java
@@ -0,0 +1,147 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datasalt.pangool.io.tuple;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.conf.Configurable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.serializer.Deserializer;
+import org.apache.hadoop.io.serializer.Serialization;
+import org.apache.hadoop.io.serializer.Serializer;
+
+import com.datasalt.pangolin.grouper.io.tuple.BaseTuple;
+import com.datasalt.pangolin.grouper.io.tuple.Tuple;
+import com.datasalt.pangool.CoGrouperException;
+import com.datasalt.pangool.PangoolConfig;
+import com.datasalt.pangool.PangoolConfigBuilder;
+import com.datasalt.pangool.Schema;
+import com.datasalt.pangool.Schema.Field;
+
+/**
+ * A {@link Serialization} for types {@link SourcedTuple}
+ * <p>
+ * To use this serialization, make sure that the Hadoop property
+ * <code>io.serializations</code> includes the fully-qualified classname of this class
+ */
+public class SourcedTupleSerialization implements Serialization<SourcedTuple>,Configurable{
+	
+	private Configuration conf;
+	private com.datasalt.pangolin.io.Serialization ser;
+	private PangoolConfig pangoolConfig;
+	
+	public SourcedTupleSerialization(){
+	}
+	
+	@Override
+  public boolean accept(Class c) {
+		return (c == Tuple.class || c == BaseTuple.class);
+  }
+
+	@Override
+	public Configuration getConf() {
+		return conf;
+	}
+	
+	@Override
+	public void setConf(Configuration thatConf) {
+		try{
+		if (thatConf != null){
+			this.conf = new Configuration(thatConf);
+			disableSourcedTupleSerialization(this.conf); // !!! MEGA TRICKY!!!!
+			
+			this.pangoolConfig = PangoolConfigBuilder.get(conf);
+			this.ser= new com.datasalt.pangolin.io.Serialization(this.conf);
+		}
+		} catch(CoGrouperException e){
+			throw new RuntimeException(e); 
+		} catch(IOException e){
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public Serializer<SourcedTuple> getSerializer(Class<SourcedTuple> c) {
+		return new SourcedTupleSerializer(this.ser,this.pangoolConfig);
+	}
+
+	@Override
+	public Deserializer<SourcedTuple> getDeserializer(Class<SourcedTuple> c) {
+		return new SourcedTupleDeserializer(this.ser,this.pangoolConfig);
+	}
+	
+	
+	 /**
+		 * Caches the values from the enum fields. This is done just once for efficiency since it uses reflection. 
+		 * 
+		 */
+		public static Map<String,Enum<?>[]> getEnums(PangoolConfig pangoolConfig) {
+			Map<String,Enum<?>[]> result = new HashMap<String,Enum<?>[]>();
+			Schema schema = pangoolConfig.getCommonOrderedSchema();
+			extractEnumsFromSchema(result, schema);
+			Map<Integer,Schema> specificSchemas = pangoolConfig.getSpecificOrderedSchemas();
+			for (Schema s : specificSchemas.values()){
+				extractEnumsFromSchema(result,s);
+			}
+			return result;
+
+		}
+		
+		public static void extractEnumsFromSchema(Map<String,Enum<?>[]> mapToFill,Schema schema){
+			try {
+				for(Field field : schema.getFields()) {
+					Class<?> type = field.getType();
+					if(type.isEnum()) {
+						Method method = type.getMethod("values",null);
+						Object values = method.invoke(null);
+						mapToFill.put(field.getName(),(Enum[])values);
+					}
+				}
+				
+			} catch(Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		
+		
+		public static void enableSourcedTupleSerialization(Configuration conf) {
+			String ser = conf.get("io.serializations").trim();
+			if (ser.length() !=0 ) {
+				ser += ",";
+			}
+			//Adding the Tuple serialization
+			ser += SourcedTupleSerialization.class.getName();
+			conf.set("io.serializations", ser);
+	  }
+		
+		/**
+		 * Mega tricky!!!!. This is to avoid recursive serialization instantiation!!
+		 * @param conf
+		 */
+		public static void disableSourcedTupleSerialization(Configuration conf){
+			String ser = conf.get("io.serializations").trim();
+			String stToSearch = Pattern.quote("," + SourcedTupleSerialization.class.getName());
+			ser = ser.replaceAll(stToSearch, "");
+			conf.set("io.serializations", ser);
+		}
+	
+}
