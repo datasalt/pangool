fc01cc082ab25182861ee35892dda34ee229183d
pere <pere@datasalt.com>
2012-01-24 17:59:48 +0100

    Missing to commit

--- core/src/main/java/com/datasalt/pangool/io/AvroUtils.java
+++ core/src/main/java/com/datasalt/pangool/io/AvroUtils.java
@@ -9,6 +9,8 @@ import org.apache.avro.generic.GenericData.Record;
 import org.apache.avro.mapred.AvroSerialization;
 import org.apache.avro.util.Utf8;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.VIntWritable;
+import org.apache.hadoop.io.VLongWritable;
 
 import com.datasalt.pangool.Schema;
 import com.datasalt.pangool.Schema.Field;
@@ -30,7 +32,22 @@ public class AvroUtils {
 			org.apache.avro.Schema fieldsSchema = null;
 			if(field.getType().equals(String.class)) {
 				fieldsSchema = org.apache.avro.Schema.create(Type.STRING);
-			}
+			} else if(field.getType().equals(Integer.class)) {
+				fieldsSchema = org.apache.avro.Schema.create(Type.INT);
+			} else if(field.getType().equals(VIntWritable.class)) {
+				fieldsSchema = org.apache.avro.Schema.create(Type.INT);
+			} else if(field.getType().equals(Long.class)) {
+				fieldsSchema = org.apache.avro.Schema.create(Type.LONG);
+			} else if(field.getType().equals(VLongWritable.class)) {
+				fieldsSchema = org.apache.avro.Schema.create(Type.LONG);
+			} else if(field.getType().equals(Float.class)) {
+				fieldsSchema = org.apache.avro.Schema.create(Type.FLOAT);
+			} else if(field.getType().equals(Double.class)) {
+				fieldsSchema = org.apache.avro.Schema.create(Type.DOUBLE);
+			} else if(field.getType().equals(Boolean.class)) {
+				fieldsSchema = org.apache.avro.Schema.create(Type.BOOLEAN);
+			} 
+			// TODO Complex types
 			avroFields.add(new org.apache.avro.Schema.Field(field.getName(), fieldsSchema, null, null));
 		}
 
@@ -44,8 +61,10 @@ public class AvroUtils {
 			Object obj = record.get(field.name());
 			if(obj instanceof Utf8) {
 				tuple.setString(field.name(), ((Utf8)obj).toString());
+			} else if(obj instanceof Comparable) {
+				tuple.setObject(field.name(), obj);
 			}
-			// etc
+			// TODO Complex types
 		}
 	}
 }
