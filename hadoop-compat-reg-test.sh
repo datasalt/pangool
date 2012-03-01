7ff04c1ebf37c52ea4d4002b973dc564c4cab167
Iván de Prado <ivan.prado@gmail.com>
2012-03-01 18:29:10 +0100

    removing mapr

--- hadoop-compat-reg-test.sh
+++ hadoop-compat-reg-test.sh
@@ -14,11 +14,11 @@ CURRENT_VERSION="0.20.2"
 LINE_NUMBER=`grep -n -A 1 "hadoop-core" $POM | cut -d '-' -f1 | tail -1`
 echo "Found hadoop-core version in $POM in line: $LINE_NUMBER"
 echo "Generating poms..."
-sed -e "${LINE_NUMBER}s/${CURRENT_VERSION}/0.20.203.0/" $POM > $POMSDIR/pom-0.20.203.0.xml
-sed -e "${LINE_NUMBER}s/${CURRENT_VERSION}/0.20.204.0/" $POM > $POMSDIR/pom-0.20.204.0.xml
-sed -e "${LINE_NUMBER}s/${CURRENT_VERSION}/0.20.205.0/" $POM > $POMSDIR/pom-0.20.205.0.xml
-sed -e "${LINE_NUMBER}s/${CURRENT_VERSION}/0.20.2-cdh3u1/" $POM > $POMSDIR/pom-0.20.2-cdh3u1.xml
-sed -e "${LINE_NUMBER}s/${CURRENT_VERSION}/0.20.2-cdh3u2/" $POM > $POMSDIR/pom-0.20.2-cdh3u2.xml
+#sed -e "${LINE_NUMBER}s/${CURRENT_VERSION}/0.20.203.0/" $POM > $POMSDIR/pom-0.20.203.0.xml
+#sed -e "${LINE_NUMBER}s/${CURRENT_VERSION}/0.20.204.0/" $POM > $POMSDIR/pom-0.20.204.0.xml
+#sed -e "${LINE_NUMBER}s/${CURRENT_VERSION}/0.20.205.0/" $POM > $POMSDIR/pom-0.20.205.0.xml
+#sed -e "${LINE_NUMBER}s/${CURRENT_VERSION}/0.20.2-cdh3u1/" $POM > $POMSDIR/pom-0.20.2-cdh3u1.xml
+#sed -e "${LINE_NUMBER}s/${CURRENT_VERSION}/0.20.2-cdh3u2/" $POM > $POMSDIR/pom-0.20.2-cdh3u2.xml
 sed -e "${LINE_NUMBER}s/${CURRENT_VERSION}/0.20.2-cdh3u3/" $POM > $POMSDIR/pom-0.20.2-cdh3u3.xml
 sed -e "${LINE_NUMBER}s/${CURRENT_VERSION}/1.0.0/" $POM > $POMSDIR/pom-1.0.0.xml
 echo "Done. Poms generated in $POMSDIR"
